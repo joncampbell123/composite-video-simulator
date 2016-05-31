606b37206a82bba06b6daffe5ab56b72075efd31
Jonathan Campbell <jonathan@castus.tv>
2016-05-31 02:35:57 -0700

    more

--- ffmpeg_to_composite.cpp
+++ ffmpeg_to_composite.cpp
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 
 extern "C" {
+#include <libavutil/opt.h>
 #include <libavutil/avutil.h>
 #include <libavutil/pixfmt.h>
 #include <libavutil/pixdesc.h>
@@ -38,13 +39,35 @@ string		input_file;
 string		output_file;
 
 AVFormatContext*	input_avfmt = NULL;
+AVStream*		input_avstream_audio = NULL;	// do not free
+AVCodecContext*		input_avstream_audio_codec_context = NULL; // do not free
+AVStream*		input_avstream_video = NULL;	// do not free
+AVCodecContext*		input_avstream_video_codec_context = NULL; // do not free
+
+struct SwrContext*	input_avstream_audio_resampler = NULL;
+
 AVFormatContext*	output_avfmt = NULL;
+AVStream*		output_avstream_audio = NULL;	// do not free
+AVCodecContext*		output_avstream_audio_codec_context = NULL; // do not free
+AVStream*		output_avstream_video = NULL;	// do not free
+AVCodecContext*		output_avstream_video_codec_context = NULL; // do not free
 
 AVRational	output_field_rate = { 60000, 1001 };	// NTSC 60Hz default
 int		output_width = 720;
 int		output_height = 480;
 bool		output_ntsc = true;	// NTSC color subcarrier emulation
 bool		output_pal = false;	// PAL color subcarrier emulation
+int		output_audio_channels = 2;	// VHS stereo (set to 1 for mono)
+int		output_audio_rate = 40000;	// VHS Hi-Fi goes up to 20KHz
+double		output_audio_linear_buzz = -42;	// how loud the "buzz" is audible in dBFS (S/N). Ever notice on old VHS tapes (prior to Hi-Fi) you can almost hear the video signal sync pulses in the audio?
+double		output_audio_highpass = 20; // highpass to filter out below 20Hz
+// NTS:
+//   VHS Hi-Fi: 20Hz - 20KHz                  (70dBFS S/N)
+//   VHS SP:    100Hz - 10KHz                 (42dBFS S/N)
+//   VHS LP:    100Hz - 7KHz (right??)        (42dBFS S/N)
+//   VHS EP:    100Hz - 4KHz                  (42dBFS S/N)
+bool		output_vhs_hifi = true;
+bool		output_vhs_linear_audio = false; // if true (non Hi-Fi) then we emulate hiss and noise of linear VHS tracks including the video sync pulses audible in the audio.
 
 void preset_PAL() {
 	output_field_rate.num = 50;
@@ -141,10 +164,148 @@ int main(int argc,char **argv) {
 		return 1;
 	}
 
+	if (avformat_find_stream_info(input_avfmt,NULL) < 0)
+		fprintf(stderr,"WARNING: Did not find stream info on input\n");
+
+	/* scan streams for one video, one audio */
+	{
+		size_t i;
+		AVStream *is;
+		AVCodecContext *isctx;
+
+		fprintf(stderr,"Input format: %u streams found\n",input_avfmt->nb_streams);
+		for (i=0;i < (size_t)input_avfmt->nb_streams;i++) {
+			is = input_avfmt->streams[i];
+			if (is == NULL) continue;
+
+			isctx = is->codec;
+			if (isctx == NULL) continue;
+
+			if (isctx->codec_type == AVMEDIA_TYPE_AUDIO) {
+				if (input_avstream_audio == NULL) {
+					if (avcodec_open2(isctx,avcodec_find_decoder(isctx->codec_id),NULL) >= 0) {
+						input_avstream_audio = is;
+						input_avstream_audio_codec_context = isctx;
+						fprintf(stderr,"Found audio stream idx=%zu\n",i);
+					}
+					else {
+						fprintf(stderr,"Found audio stream but not able to decode\n");
+					}
+				}
+			}
+			else if (isctx->codec_type == AVMEDIA_TYPE_VIDEO) {
+				if (avcodec_open2(isctx,avcodec_find_decoder(isctx->codec_id),NULL) >= 0) {
+					input_avstream_video = is;
+					input_avstream_video_codec_context = isctx;
+					fprintf(stderr,"Found video stream idx=%zu\n",i);
+				}
+				else {
+					fprintf(stderr,"Found video stream but not able to decode\n");
+				}
+			}
+		}
+
+		if (input_avstream_video == NULL && input_avstream_audio == NULL) {
+			fprintf(stderr,"Neither video nor audio found\n");
+			return 1;
+		}
+	}
+
 	assert(output_avfmt == NULL);
 	if (avformat_alloc_output_context2(&output_avfmt,NULL,NULL,output_file.c_str()) < 0) {
 		fprintf(stderr,"Failed to open output file\n");
 		return 1;
+	}
+
+	if (input_avstream_audio != NULL) {
+		output_avstream_audio = avformat_new_stream(output_avfmt, NULL);
+		if (output_avstream_audio == NULL) {
+			fprintf(stderr,"Unable to create output audio stream\n");
+			return 1;
+		}
+
+		output_avstream_audio_codec_context = output_avstream_audio->codec;
+		if (output_avstream_audio_codec_context == NULL) {
+			fprintf(stderr,"Output stream audio no codec context?\n");
+			return 1;
+		}
+
+		if (output_audio_channels == 2)
+			output_avstream_audio_codec_context->channel_layout = AV_CH_LAYOUT_STEREO;
+		else
+			output_avstream_audio_codec_context->channel_layout = AV_CH_LAYOUT_MONO;
+
+		output_avstream_audio_codec_context->sample_rate = output_audio_rate;
+		output_avstream_audio_codec_context->channels = output_audio_channels;
+		output_avstream_audio_codec_context->sample_fmt = AV_SAMPLE_FMT_S16;
+		output_avstream_audio_codec_context->time_base = (AVRational){1, output_audio_rate};
+		output_avstream_audio->time_base = output_avstream_audio_codec_context->time_base;
+
+		if (output_avfmt->oformat->flags & AVFMT_GLOBALHEADER)
+			output_avstream_audio_codec_context->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+
+		if (avcodec_open2(output_avstream_audio_codec_context,avcodec_find_encoder(AV_CODEC_ID_PCM_S16LE),NULL) < 0) {
+			fprintf(stderr,"Output stream cannot open codec\n");
+			return 1;
+		}
+
+		input_avstream_audio_resampler = swr_alloc();
+		av_opt_set_int(input_avstream_audio_resampler, "in_channel_layout", input_avstream_audio_codec_context->channel_layout, 0);
+		av_opt_set_int(input_avstream_audio_resampler, "out_channel_layout", output_avstream_audio_codec_context->channel_layout, 0);
+		av_opt_set_int(input_avstream_audio_resampler, "in_sample_rate", input_avstream_audio_codec_context->sample_rate, 0);
+		av_opt_set_int(input_avstream_audio_resampler, "out_sample_rate", output_avstream_audio_codec_context->sample_rate, 0);
+		av_opt_set_sample_fmt(input_avstream_audio_resampler, "in_sample_fmt", input_avstream_audio_codec_context->sample_fmt, 0);
+		av_opt_set_sample_fmt(input_avstream_audio_resampler, "out_sample_fmt", output_avstream_audio_codec_context->sample_fmt, 0);
+		if (swr_init(input_avstream_audio_resampler) < 0) {
+			fprintf(stderr,"Failed to init audio resampler\n");
+			return 1;
+		}
+
+		fprintf(stderr,"Audio resampler init %uHz -> %uHz\n",
+			input_avstream_audio_codec_context->sample_rate,
+			output_avstream_audio_codec_context->sample_rate);
+	}
+
+	if (input_avstream_video != NULL) {
+		output_avstream_video = avformat_new_stream(output_avfmt, NULL);
+		if (output_avstream_video == NULL) {
+			fprintf(stderr,"Unable to create output video stream\n");
+			return 1;
+		}
+
+		output_avstream_video_codec_context = output_avstream_video->codec;
+		if (output_avstream_video_codec_context == NULL) {
+			fprintf(stderr,"Output stream video no codec context?\n");
+			return 1;
+		}
+
+		avcodec_get_context_defaults3(output_avstream_video_codec_context,avcodec_find_encoder(AV_CODEC_ID_H264));
+		output_avstream_video_codec_context->width = output_width;
+		output_avstream_video_codec_context->height = output_height;
+		output_avstream_video_codec_context->sample_aspect_ratio = (AVRational){output_height*4, output_width*3};
+		output_avstream_video_codec_context->pix_fmt = AV_PIX_FMT_YUV422P;
+		output_avstream_video_codec_context->time_base = (AVRational){output_field_rate.den, (output_field_rate.num/2)}; // NTS: divide by 2 to convert fields -> frames
+		output_avstream_video_codec_context->gop_size = 15;
+		output_avstream_video_codec_context->max_b_frames = 0;
+		output_avstream_video->time_base = output_avstream_video_codec_context->time_base;
+
+		if (output_avfmt->oformat->flags & AVFMT_GLOBALHEADER)
+			output_avstream_video_codec_context->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+
+		// our output is interlaced
+		output_avstream_video_codec_context->flags |= CODEC_FLAG_INTERLACED_DCT;
+
+		if (avcodec_open2(output_avstream_video_codec_context,avcodec_find_encoder(AV_CODEC_ID_H264),NULL) < 0) {
+			fprintf(stderr,"Output stream cannot open codec\n");
+			return 1;
+		}
+	}
+
+	if (!(output_avfmt->oformat->flags & AVFMT_NOFILE)) {
+		if (avio_open(&output_avfmt->pb, output_file.c_str(), AVIO_FLAG_WRITE) < 0) {
+			fprintf(stderr,"Output file cannot open file\n");
+			return 1;
+		}
 	}
 
 	if (avformat_write_header(output_avfmt,NULL) < 0) {
@@ -153,8 +314,11 @@ int main(int argc,char **argv) {
 	}
 
 	av_write_trailer(output_avfmt);
+	if (input_avstream_audio_resampler != NULL)
+		swr_free(&input_avstream_audio_resampler);
+	if (output_avfmt != NULL && !(output_avfmt->oformat->flags & AVFMT_NOFILE))
+		avio_closep(&output_avfmt->pb);
 	avformat_free_context(output_avfmt);
-	av_free(output_avfmt);
 	avformat_close_input(&input_avfmt);
 	return 0;
 }
