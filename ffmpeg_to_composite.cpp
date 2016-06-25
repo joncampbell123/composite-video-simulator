c08723e02097bd568bdab8397bf665b35d47635e
Jonathan Campbell <jonathan@castus.tv>
2016-06-25 05:15:44 -0700

    Make output status something that rewrites one line. Factor packet duration into rendering. Sadly, video is kinda jumpy coming directly from VOB files.

--- ffmpeg_to_composite.cpp
+++ ffmpeg_to_composite.cpp
@@ -1059,7 +1059,7 @@ void output_frame(AVFrame *frame,unsigned long long field_number,unsigned int fi
 		pkt.dts = field_number;
 	}
 
-	fprintf(stderr,"Output field %llu\n",field_number);
+	fprintf(stderr,"\x0D" "Output field %llu ",field_number); fflush(stderr);
 	if (output_video_as_interlaced && use_422_colorspace) { // 4:2:2 interlaced = use as-is
         if (avcodec_encode_video2(output_avstream_video_codec_context,&pkt,frame,&gotit) == 0) {
             if (gotit) {
@@ -1846,30 +1846,15 @@ int main(int argc,char **argv) {
 						if (tgt_field == AV_NOPTS_VALUE) tgt_field = input_avstream_video_frame->pkt_dts;
 						if (tgt_field == AV_NOPTS_VALUE) tgt_field = pkt.dts;
 
+                        if (pkt.duration > 0)
+                            tgt_field += pkt.duration;
+
 						if (output_avstream_video_input_frame != NULL) {
 							if (output_avstream_video_input_frame->height != input_avstream_video_frame->height) {
                                 if (output_avstream_video_input_frame != NULL)
                                     av_frame_free(&output_avstream_video_input_frame);
                             }
                         }
-
-						if (output_avstream_video_input_frame != NULL) {
-							while (video_field < tgt_field) {
-								render_field(output_avstream_video_frame,output_avstream_video_input_frame,(int)(video_field & 1ULL) ^ 1/*bottom field first*/,video_field);
-
-                                if (enable_composite_emulation)
-                                    composite_video_process(output_avstream_video_frame,(int)(video_field & 1ULL) ^ 1/*bottom field first*/,video_field);
-
-                                if (output_video_as_interlaced) {
-                                    if ((video_field & 1ULL)) output_frame(output_avstream_video_frame,video_field - 1ULL,(int)((video_field - 1ULL) & 1ULL) ^ 1/*bottom field first*/);
-                                }
-                                else {
-                                    output_frame(output_avstream_video_frame,video_field,(int)(video_field & 1ULL) ^ 1/*bottom field first*/);
-                                }
-
-                                video_field++;
-							}
-						}
 
 						if (output_avstream_video_input_frame == NULL) {
 							fprintf(stderr,"New input frame\n");
